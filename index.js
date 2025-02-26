031db162e86d0cad930d67bfd2f505677a9b02ba
Bin Mohamamd Technologies <binmohammadtech@gmail.com>
2025-02-26 17:14:56 +0500

    second commit

--- index.js
+++ index.js
@@ -5,4 +5,39 @@ const app = express();
 
 app.use(urlencoded({extended : true}));
 app.use(cors());
-app.use(bodyParser);
+
+
+
+
+
+// api routes
+app.get('/', (req, res) =>{
+
+
+  return  res.json({success : true, message : 'successfully reached api call!'});
+
+
+
+});
+app.get('/welcome', (req, res) =>{
+
+
+   return res.json({success : true, message : 'this is the welcome route'});
+
+});
+
+
+
+
+
+
+
+
+
+
+const PORT = 5005;
+app.listen(PORT, () =>{
+
+    console.log(`server started successfully at http://localhost:${PORT}`)
+
+}); 
